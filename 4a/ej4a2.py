c4d961849d1833264c459f1f46c460cc127840c8
jdelegon-wq <jdelegon@uoc.edu>
2025-12-13 18:46:08 +0100

    Implement count_fruits function to count items

--- 4a/ej4a2.py
+++ 4a/ej4a2.py
@@ -45,8 +45,15 @@ Exemple:
 
 def count_fruits(fruits_list):
     # Write here your code
-    pass
+    fruits_dicc = {}
+    for element in fruits_list:
+        if element in fruits_dicc:
+            fruits_dicc[element] += 1
+        else:
+            fruits_dicc[element] = 1
+return fruits_dicc
 
+    
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
