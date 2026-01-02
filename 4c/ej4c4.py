f3484f5d6e209370ad7d4fb313a47f4faa8e44fa
jdelegon-wq <jdelegon@uoc.edu>
2026-01-02 14:44:52 +0100

    Implement Animal class and subclasses for Dog, Cat, Parrot

--- 4c/ej4c4.py
+++ 4c/ej4c4.py
@@ -55,28 +55,35 @@ Exemple:
          "Polly"        
 """
 # Write abstract class Animal here
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+	@abstractmethod
+	def talk(self):
+		pass
 
 # Corret and overwrite class Dog(Animal) here 
-class Dog():
+class Dog(Animal):
     def __init__(self, name):
         self.name = name
 
     def talk(self):
-        pass
+		print("Guau!")
 
+		
 # Corret and overwrite class Cat(Animal) here 
-class Cat():
+class Cat(Animal):
     def __init__(self, name):
         self.name = name
     def talk(self):
-        pass
+        print("Meow!")
 
 # Corret and overwrite class Parrot(Animal) here 
-class Parrot():
+class Parrot(Animal):
     def __init__(self, name):
         self.name = name
     def talk(self):
-        pass
+        print("Whistle!")
 
 
 
